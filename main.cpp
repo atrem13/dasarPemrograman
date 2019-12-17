cbf7eee6afa5f3188b2939d25066f9a7089695fc
atrem13 <31754826+atrem13@users.noreply.github.com>
2019-12-17 09:24:35 +0800

    Add files via upload
    test upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // program A
+//    int x, z;
+//    float y, u;
+//    x = 12;
+//    y = 2.15;
+//    z = x*y;
+//
+//    cout << "X = "<< x << endl;
+//    cout << "Y = "<< y << endl;
+//    cout << "Z = "<< z << endl;
+
+    //Program B
+//    int x;
+//    float y, z;
+//    x = 12.8;
+//    y = 2.15;
+//    z = x*y;
+//    cout << "X = "<< x << endl;
+//    cout << "Y = "<< y << endl;
+//    cout << "Z = "<< z << endl;
+
+    //Program C
+//    int alas, tinggi;
+//    float luas;
+//    cout << "Masukan Alas : ";
+//    cin >> alas;
+//    cout << "\nMasukan Tinggi : ";
+//    cin >> tinggi;
+//    luas = alas * tinggi / 2;
+//    cout << "\n\nHasil Dari " << alas << " x " << tinggi << " / 2 = " << luas;
+
+    //Program D
+    int p,l,h;
+    cout << "Masukan Panjang Persegi : ";
+    cin >> p;
+    cout << "\nMasukan Lebar Persegi : ";
+    cin >> l;
+    h = p*l;
+    cout << "\n\nLuas Persegi Panjang = "<<h;
+    return 0;
+}
